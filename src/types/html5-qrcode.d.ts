d79cefb09421c935b8389251489a80cd90ce9362
Eva BARDEAU <evaw.bardeau@gmail.com>
2025-05-21 17:52:03 +0200

    Optimize camera for IOS

--- src/types/html5-qrcode.d.ts
+++ src/types/html5-qrcode.d.ts
@@ -1,4 +1,39 @@
 declare module 'html5-qrcode' {
+  export interface CameraDevice {
+    id: string;
+    label: string;
+  }
+
+  export interface QrboxSize {
+    width: number;
+    height: number;
+  }
+
+  export interface QrConfig {
+    fps?: number;
+    qrbox?: number | QrboxSize;
+    aspectRatio?: number;
+    disableFlip?: boolean;
+    formatsToSupport?: string[];
+  }
+
+  export class Html5Qrcode {
+    constructor(elementId: string);
+    
+    static getCameras(): Promise<CameraDevice[]>;
+    
+    start(
+      deviceId: string,
+      config: QrConfig,
+      successCallback: (decodedText: string) => void,
+      errorCallback?: (errorMessage: string) => void
+    ): Promise<void>;
+    
+    stop(): Promise<void>;
+    
+    clear(): void;
+  }
+
   export class Html5QrcodeScanner {
     constructor(
       elementId: string,
